2a26532a6144e84c62124e8335152caa98cf59ca
Ambika1210 <ambika.ambika1210@gmail.com>
2025-10-25 18:08:56 +0530

    deleteUpdate

--- src/routes/filledForm.route.js
+++ src/routes/filledForm.route.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { createFormController,getAllFormController } = require("../controller/filledForm.controller");
+const { createFormController,getAllFormController,updateFormController, deleteFormController} = require("../controller/filledForm.controller");
 
 router.post("/create-new-form",createFormController)
 router.get("/get-all-form",getAllFormController)
+router.patch("/update-form/:id",updateFormController)
+router.delete("/delete-form/:id",deleteFormController)
+
+
 module.exports = router;
 
 
